8e95f6f22b252ef338cc2ec58535d5949081f5c6
usaid <mdusaid922@gmail.com>
2023-08-12 20:23:38 +0500

    working on schedule enter

--- src/components/Schedule/scheduleenter.js
+++ src/components/Schedule/scheduleenter.js
@@ -5,7 +5,8 @@ import { useState,useEffect } from "react";
 
 
 function Scheduleenter({close,unformatted}){
-    const [selectedschedule,setselectedschedule]=useState('')
+    const [selectedschedule,setselectedschedule]=useState({name:'',address:'',fee:'',availability:[{day:'',time:['']}]})
+    const [selectedscheduleindex,setselectedscheduleindex]=useState(0)
     const [editschedule,seteditschedule]=useState([])
 const handleinput=(e)=>{}
 
@@ -13,8 +14,8 @@ useEffect(() => {geteditschedule()
 }, []);
 
 useEffect(() => {console.log(editschedule)
-}, [editschedule]);
-function convertschedule(){}
+}, [editschedule])
+
 function geteditschedule() {
     try {
         const params = sessionStorage.getItem('org_name') + '/' + sessionStorage.getItem('email');
@@ -29,10 +30,28 @@ function geteditschedule() {
 
             if (json.schedule) {
                 seteditschedule(json.schedule);
-                setselectedschedule(json.schedule[0].name);
+                setselectedschedule({name:json.schedule[0].name,address:json.schedule[0].address,fee:json.schedule[0].fee,availability:[
+                    {day:'Monday',time:(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='monday'}))?(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='monday'})).time:['']},
+                    {day:'Tuesday',time:(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='tuesday'}))?(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='tuesday'})).time:['']},
+                    {day:'Wednesday',time:(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='wednesday'}))?(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='wednesday'})).time:['']},
+                    {day:'Thursday',time:(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='thursday'}))?(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='thursday'})).time:['']},
+                    {day:'Friday',time:(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='friday'}))?(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='friday'})).time:['']},
+                    {day:'Saturday',time:(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='saturday'}))?(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='saturday'})).time:['']},
+                    {day:'Sunday',time:(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='sunday'}))?(json.schedule[0].availability.filter(a=>{a.day.toLowerCase()==='sunday'})).time:['']}
+                
+                ]});
                 console.log(json.schedule);
             } else {
-                seteditschedule([]);
+                seteditschedule([{name:'Unnamed',address:'',fee:'',availability:[]}]);
+                setselectedschedule({name:'Unnamed',address:'',fee:'',availability:[
+                    {day:'Monday',time:['']},
+                    {day:'Tuesday',time:['']},
+                    {day:'Wednesday',time:['']},
+                    {day:'Thursday',time:['']},
+                    {day:'Friday',time:['']},
+                    {day:'Saturday',time:['']},
+                    {day:'Sunday',time:['']},
+                ]})
             }
             if (json.error) {
                 console.log(json.error);
@@ -51,7 +70,25 @@ const addhospital=()=>{
 
 }
 
-
+const changeselectedhospital=(e)=>{
+    document.getElementsByClassName('actiivehospitaltab')[0].classList.remove('actiivehospitaltab')
+    e.target.classList.add('actiivehospitaltab')
+    setselectedscheduleindex(e.target.id)
+    const element=editschedule[e.target.id]
+    setselectedschedule({
+        name:element.name,address:element.address,fee:element.fee,availability:[
+            {day:'Monday',time:(element.availability.filter(a=>{a.day.toLowerCase()==='monday'}))?(element.availability.filter(a=>{a.day.toLowerCase()==='monday'})).time:['']},
+            {day:'Tuesday',time:(element.availability.filter(a=>{a.day.toLowerCase()==='tuesday'}))?(element.availability.filter(a=>{a.day.toLowerCase()==='tuesday'})).time:['']},
+            {day:'Wednesday',time:(element.availability.filter(a=>{a.day.toLowerCase()==='wednesday'}))?(element.availability.filter(a=>{a.day.toLowerCase()==='wednesday'})).time:['']},
+            {day:'Thursday',time:(element.availability.filter(a=>{a.day.toLowerCase()==='thursday'}))?(element.availability.filter(a=>{a.day.toLowerCase()==='thursday'})).time:['']},
+            {day:'Friday',time:(element.availability.filter(a=>{a.day.toLowerCase()==='friday'}))?(element.availability.filter(a=>{a.day.toLowerCase()==='friday'})).time:['']},
+            {day:'Saturday',time:(element.availability.filter(a=>{a.day.toLowerCase()==='saturday'}))?(element.availability.filter(a=>{a.day.toLowerCase()==='saturday'})).time:['']},
+            {day:'Sunday',time:(element.availability.filter(a=>{a.day.toLowerCase()==='sunday'}))?(element.availability.filter(a=>{a.day.toLowerCase()==='sunday'})).time:['']}
+                
+        ]
+    })
+    
+}
 
 const addtimevalue=(e)=>{}
 return(<>
@@ -69,7 +106,7 @@ return(<>
 {editschedule.map((i,index)=>{
     
         return(<>
-                        <div className={index===0?"actiivehospitaltab":''}><h2>{i.name}</h2></div>
+                        <div className={index===0?"actiivehospitaltab":''} onClick={changeselectedhospital} id={index}><h2 id={index}>{i.name}</h2></div>
 
         </>)
 })}
